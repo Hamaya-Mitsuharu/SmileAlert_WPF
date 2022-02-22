203f28532b7e723b87e8ae94a36c795ea8f25017
Hamaya-Mitsuharu <NishinNishin0@gmail.com>
2022-02-22 19:08:56 +0900

    Editing POST request

--- SmileAlert/MainWindow.xaml.cs
+++ SmileAlert/MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,6 +19,7 @@ using OpenCvSharp;
 using OpenCvSharp.Extensions;
 using System.Threading;
 
+
 namespace SmileAlert
 {
     /// <summary>
@@ -23,7 +27,8 @@ namespace SmileAlert
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
-        Mat frame;
+        Mat matFrame;
+        private const string ATTRIBUTES = "gender,age";
 
         Task task;
         public MainWindow()
@@ -35,53 +40,110 @@ namespace SmileAlert
         async Task Count()
         {
             var captureCount = 0;
-            await Task.Run(() =>
-            {
-                //
-                // カメラを起動
-                //
-                using (var capture = new VideoCapture())
-                {
-                    capture.Open(0);
+            await Task.Run(async () =>
+           {
+               //
+               // カメラを起動
+               //
+               using (var capture = new VideoCapture())
+               {
+                   capture.Open(0);
 
-                    if (!capture.IsOpened())
-                    {
-                        throw new Exception("capture initialization failed");
-                    }
+                   if (!capture.IsOpened())
+                   {
+                       throw new Exception("capture initialization failed");
+                   }
 
-                    while (true)
-                    {
-                        //
-                        // キャプチャ回数をカウント
-                        //
-                        captureCount++;
-                        ResultLabel.Dispatcher.BeginInvoke(
-                            new Action(() =>
-                            {
-                                ResultLabel.Content = captureCount.ToString();
-                            })
-                        );
+                   while (true)
+                   {
+                       //
+                       // キャプチャ回数をカウント
+                       //
+                       /*
+                       captureCount++;
+                       await ResultLabel.Dispatcher.BeginInvoke(
+                           new Action(() =>
+                           {
+                               ResultLabel.Content = captureCount.ToString();
+                           })
+                       );
+                       */
 
-                        //
-                        // キャプチャ
-                        //
-                        frame = new Mat();
-                        capture.Read(frame);
-                        if (frame.Empty())
-                        {
-                            // return null;
-                        }
-                        Monitor.Dispatcher.BeginInvoke(
-                            new Action(() =>
-                            {
-                                Monitor.Source = MatToImageSource(frame);
-                            })
-                        );
+                       //
+                       // キャプチャ
+                       //
+                       matFrame = new Mat();
+                       capture.Read(matFrame);
+                       if (matFrame.Empty())
+                       {
+                           // return null;
+                       }
 
-                        Thread.Sleep(100);
-                    }
-                }
-            });
+                       //
+                       // Mat -> Bytes -> Base64
+                       //
+                       var bytesImg = matFrame.ToBytes();
+                       var base64Img = Convert.ToBase64String(bytesImg);
+
+                       var client = new HttpClient();
+                       // var content = new StringContent(base64Img);
+
+                       // HTTPヘッダの設定
+                       var url = "https://api-us.faceplusplus.com/facepp/v3/detect";
+                       var API_KEY = "gvtqsN4gF5kfSQwRm3bEbFYgSSsrLGwH";
+                       var API_SECRET = "52HOOEt4fb1-JAI5kHjRDWNPSBkFOyK3";
+                       
+                       // request.Headers.Add("Content-Type", "multipart/form-data; boundary=----WebKitFormBoundaryxxxxyz");
+                       // request.Headers.Add("api_key", API_KEY);
+                       // request.Headers.Add("api_secret", API_SECRET);
+                       // request.Headers.Add("image_base64", base64Img);
+                       // request.Headers.Add("return_attributes", ATTRIBUTES);
+
+                       string res = "";
+
+                       var contentDict = new Dictionary<string, string>();
+                       contentDict.Add("api_key", API_KEY);
+                       contentDict.Add("api_secret", API_SECRET);
+                       contentDict.Add("image_base64", base64Img);
+
+                       using (var request = new HttpRequestMessage(HttpMethod.Post, url))
+                       using (var jsonContent = JsonContent.Create(contentDict))
+                       {
+                           request.Content = jsonContent;
+                           request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("multipart/form-data");
+                           // request.Content.Headers.Add("api_key", API_KEY);
+                           // request.Content.Headers.Add("api_secret", API_SECRET);
+
+                           var result = await client.SendAsync(request);
+                           var status = result.StatusCode.ToString();
+                           var json = await result.Content.ReadAsStringAsync();
+                           var isSuccess = result.IsSuccessStatusCode.ToString();
+                           res = result.ToString() + "\n" + status + "\n" + json + "\n" + isSuccess;
+                       }
+                       
+                       await ResultLabel.Dispatcher.BeginInvoke(
+                           new Action(() =>
+                           {
+                               ResultLabel.Content = res;
+                           })
+                       );
+
+
+                       //
+                       // 画面に表示
+                       //
+                       await Monitor.Dispatcher.BeginInvoke(
+                          new Action(() =>
+                          {
+                              Monitor.Source = MatToImageSource(matFrame);
+                          })
+                      );
+                       matFrame.Dispose();
+
+                       Thread.Sleep(1000);
+                   }
+               }
+           });
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -94,8 +156,8 @@ namespace SmileAlert
         void UpdateImage()
         {
             Capture();
-            Monitor.Source = MatToImageSource(frame);
-            frame.Dispose();
+            Monitor.Source = MatToImageSource(matFrame);
+            matFrame.Dispose();
         }
 
         /// <summary>
@@ -118,9 +180,9 @@ namespace SmileAlert
                 //
                 // フレーム画像を取得
                 //
-                frame = new Mat();
-                capture.Read(frame);
-                if (frame.Empty())
+                matFrame = new Mat();
+                capture.Read(matFrame);
+                if (matFrame.Empty())
                 {
                     // return null;
                 }
